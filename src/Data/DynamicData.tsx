6afc70236b67fc9c3f860f5e76a2ef0a6cac0723
hadyshref <hadyshref2@gmail.com>
2026-02-05 14:36:41 +0200

    update videos

--- src/Data/DynamicData.tsx
+++ src/Data/DynamicData.tsx
@@ -2,22 +2,22 @@ import { Mail, Phone, Instagram } from "lucide-react";
 
 export const reelsData = [
     { id: 1, title: "before & after", video: "https://streamable.com/zawsuj" },
-    { id: 2, title: "reels", video: "./assets/reel2.mp4" },
-    { id: 3, title: "Reel 3", video: "./assets/reel3.mp4" },
-    { id: 4, title: "Reel 4", video: "./assets/reel4.mp4" },
-    { id: 5, title: "Reel 5", video: "./assets/reel5.mp4" },
-    { id: 6, title: "Reel 6", video: "./assets/reel6.mp4" },
-    { id: 7, title: "Reel 7", video: "./assets/reel7.mp4" },
-    { id: 8, title: "Reel 8", video: "./assets/reel8.mp4" },
-    { id: 9, title: "Reel 9", video: "./assets/reel9.mp4" },
-    { id: 10, title: "Reel 10", video: "./assets/reel10.mp4" },
+    { id: 2, title: "reels", video: "https://streamable.com/vopy4a" },
+    { id: 3, title: "Reel 3", video: "https://streamable.com/1lz5gq" },
+    { id: 4, title: "Reel 4", video: "https://streamable.com/hi07hg" },
+    { id: 5, title: "Reel 5", video: "https://streamable.com/euzxm9" },
+    { id: 6, title: "Reel 6", video: "https://streamable.com/rlv9xd" },
+    { id: 7, title: "Reel 7", video: "https://streamable.com/8q3iyh" },
+    { id: 8, title: "Reel 8", video: "https://streamable.com/fpnpjv" },
+    { id: 9, title: "Reel 9", video: "https://streamable.com/0epjnc" },
+    { id: 10, title: "Reel 10", video: "https://streamable.com/7s1s3g" },
 ];
 
 export const videos = [
-    { id: 1, title: "Personal Brand Film", tag: "Spec Work", src: "/assets/video1.mp4" },
-    { id: 2, title: "Motion Design Reel", tag: "Motion Graphics", src: "/assets/video1.mp4" },
-    { id: 3, title: "Short Documentary Cut", tag: "Editing", src: "/assets/video1.mp4" },
-    { id: 4, title: "Product Launch Film", tag: "Commercial", src: "/assets/video1.mp4" },
+    { id: 1, title: "binharmal", tag: "mudospace", src: "https://streamable.com/exuysz" },
+    { id: 2, title: "Motion Design Reel", tag: "Motion Graphics", src: "https://streamable.com/621s9i" },
+    { id: 3, title: "Short Documentary Cut", tag: "Editing", src: "https://streamable.com/4lq7d7" },
+    { id: 4, title: "Product Launch Film", tag: "Commercial", src: "https://streamable.com/pjg9x6" },
 ];
 
 export const contacts = [
