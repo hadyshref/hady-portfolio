8d1a1927d66adb16e580c3d18608cfa15d5152ee
hadyshref <hadyshref2@gmail.com>
2026-02-05 19:38:15 +0200

    سس

--- src/Data/DynamicData.tsx
+++ src/Data/DynamicData.tsx
@@ -6,7 +6,7 @@ export const reelsData = [
     { id: 3, title: "Reel 3", video: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770296169/8q3iyh_iygbnj.mp4" },
     { id: 4, title: "Reel 4", video: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770310389/Img_9442_oq8lho.mp4" },
     { id: 5, title: "Reel 5", video: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770308975/IMG_5308_mwufpy.mp4" },
-    { id: 6, title: "Reel 6", video: "https://streamable.com/rlv9xd" },
+    { id: 6, title: "Reel 6", video: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770312970/Sequence_02_zlbypf.mp4" },
     { id: 7, title: "Reel 7", video: "https://streamable.com/8q3iyh" },
     { id: 8, title: "Reel 8", video: "https://streamable.com/fpnpjv" },
     { id: 9, title: "Reel 9", video: "https://streamable.com/0epjnc" },
@@ -17,7 +17,7 @@ export const videos = [
     { id: 1, title: "binharmal", tag: "mudospace", src: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770308479/-1_%D8%B5%D9%82%D8%B1-_a5a1ri.mp4" },
     { id: 2, title: "TEETH", tag: "Motion Graphics", src: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770308406/Img_5405_c6pgpo.mp4" },
     { id: 3, title: "Short Documentary Cut", tag: "Editing", src: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770310589/Img_8710_ljzenh.mp4" },
-    { id: 4, title: "Product Launch Film", tag: "Commercial", src: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770296203/621s9i_bqumvv.mp4" },
+    { id: 4, title: "Product Launch Film", tag: "Commercial", src: "https://res.cloudinary.com/dvifw6i2x/video/upload/v1770313038/Img_7755_eqmsqh.mp4" },
 ];
 
 export const contacts = [
